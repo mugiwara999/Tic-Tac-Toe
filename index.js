599a863c4c48ab0b373df13685fca9219372cb61
mugiwara999 <125131353+mugiwara999@users.noreply.github.com>
2024-05-21 19:31:15 +0530

    fix checkForWin function

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const gameControl = function() {
     value1 = gameBoard[0][2]
     value2 = gameBoard[1][1]
     value3 = gameBoard[2][0]
+    console.log(value1)
+    console.log(value2)
+    console.log(value3)
 
     if ((value1 === value2) && (value2 === value3)) {
       return true
@@ -27,6 +30,18 @@ const gameControl = function() {
     if ((value1 === value2) && (value2 === value3)) {
       return true
     }
+
+    console.log(value1)
+    console.log(value2)
+    console.log(value3)
+
+    console.log(checkInDirection("side", 0, 0, value1))
+    console.log(checkInDirection("vertical", 0, 0, value1))
+    console.log(checkInDirection("side", 1, 1, value2))
+    console.log(checkInDirection("vertical", 1, 1, value2))
+    console.log(checkInDirection("side", 2, 2, value3))
+    console.log(checkInDirection("vertical", 2, 2, value3))
+
     if (checkInDirection("side", 0, 0, value1) || checkInDirection("vertical", 0, 0, value1) || checkInDirection("side", 1, 1, value2) || checkInDirection("vertical", 1, 1, value2) || checkInDirection("side", 2, 2, value3) || checkInDirection("vertical", 2, 2, value3)) {
 
       return true
@@ -40,7 +55,10 @@ const gameControl = function() {
     coordinates = [0, 1, 2]
     if (motion == "side") {
       temp = coordinates;
-      temp.slice(i, 1);
+      temp.splice(i, 1);
+      console.log(temp)
+      console.log(gameBoard[i][temp[0]])
+      console.log(gameBoard[i][temp[1]])
 
       if ((value == gameBoard[i][temp[0]]) && (value == gameBoard[i][temp[1]])) {
         return true;
@@ -51,7 +69,10 @@ const gameControl = function() {
 
     if (motion == "vertical") {
       temp = coordinates;
-      temp.slice(i, 1);
+      temp.splice(i, 1);
+      console.log(temp)
+      console.log(gameBoard[temp[0]][j])
+      console.log(gameBoard[temp[1]][j])
 
       if ((value === gameBoard[temp[0]][j]) && (value === gameBoard[temp[1]][j])) {
         return true;
